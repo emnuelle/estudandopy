2c165e929171f81ae9b0fcc687bfe31863bf6707
emnuelle <rm97973@fiap.com.br>
2023-03-30 13:47:21 -0300

    adc mais uma prática

---
+++ ex11.py
@@ -0,0 +1,11 @@
+#  Peça para o usuário o valor do RAIO de um
+# círculo e exiba a área deste círculo .
+# (lembrete: área do círculo = π . raio2)
+
+print("Vamos exibir a área de um círculo, para isso, basta apenas você digitar o raio dele!")
+
+raio = float(input())
+
+area = 3.14 * (raio * 2)
+
+print("A área do círculo cujo raio é", raio, "é:", area)
