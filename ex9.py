ec4b8508bc3a1de4bfa6979f3262918a72326523
emnuelle <rm97973@fiap.com.br>
2023-03-30 13:33:59 -0300

    adicionando mais um exercício

---
+++ ex9.py
@@ -0,0 +1,16 @@
+#   Peça para o usuário digitar as notas das 4
+# provas (p1, p2, p3 e p4) e exiba a média aritmética simples
+
+print("Vamos descobrir a médias, coloque suas notas: ")
+
+n1 = input()
+
+n2 = input()
+
+n3 = input()
+
+n4 = input()
+
+resultado = (float(n1) + float(n2) + float(n3) + float(n4)) / 4
+
+print("Seu resultado é:", str(resultado))
