88b6f74261c22a15ae7486f2f2bdb57b2a537a1f
emnuelle <rm97973@fiap.com.br>
2023-03-30 13:41:12 -0300

    Adc ex n10!

---
+++ ex10.py
@@ -0,0 +1,17 @@
+#  Peça para o usuário digitar o ano atual e o ano
+# de seu nascimento e exiba a idade.
+
+print("Vamos descobrir a sua idade através destes dados:")
+
+print("Digite o ano do seu nascimento:")
+nasc = int(input())
+
+print("Em qe ano nós estamos? Digite. ")
+ano = int(input())
+
+idade = ano - nasc
+
+print("Hoje você tem", idade, "anos!")
+
+
+
