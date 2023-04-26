083f021916b23b2c8ee7b8dfcefaacea0c6d63e5
emnuelle <rm97973@fiap.com.br>
2023-04-26 16:40:50 -0300

    21

---
+++ ex16.py
@@ -0,0 +1,16 @@
+# Peça para o usuário digitar se é fã de Star Wars ou 
+# Star Trek. Exiba “Que a força esteja com você” se for fã de 
+# Star Wars ou “Vida Longa e Próspera” se for fã de Star 
+# Trek 
+
+print("Star Wars ou Star Trek")
+
+filme = str.upper(input())
+
+if filme == "Star Wars".upper() :
+    print("Que a força esteja com você!")
+else :
+    print("Vida longa e próspera")
+
+
+#  Foi mas não entendi muito bem blé 
---
+++ ex17.py
@@ -0,0 +1,5 @@
+# peça para o usuário digitar a média de 
+# “matemática” e exiba se ele foi aprovado (nota maior ou 
+# igual a 6) ou reprovado (nota inferior a 6).
+
+Print("Vamos descobrir a média de matemática")
