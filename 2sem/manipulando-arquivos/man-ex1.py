01e08da1adc2408aa4cced250f2b5c92ccfb8c40
emnuelle <rm97973@fiap.com.br>
2023-09-04 10:46:56 -0300

    lerolero

---
+++ 2sem/manipulando-arquivos/man-ex1.py
@@ -0,0 +1,6 @@
+f = open('lero.txt','r', encoding='utf-8')
+print(f.read())
+f.close()
+
+# supostamente deveria printar o poema da baatinha :(
+# funcionou no pycharm!!!!!!! (pasta no dados(D))
---
+++ 2sem/manipulando-arquivos/man1.py
@@ -0,0 +1,9 @@
+# A função chave para se manipular um arquivo é open(nome/modo de edição)
+
+# 4 modos diferentes para abrir um arquivo
+# "r" - read
+#  "a" - append
+#  "w" -  write
+# "c" create
+
+#  Se abrir o arquivo, sempre fechar ele
