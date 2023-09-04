31df482ed180147c55ca42f0cbc3fa8e7d4d8686
emnuelle <rm97973@fiap.com.br>
2023-09-04 11:03:25 -0300

    ver2

--- 2sem/manipulando-arquivos/man-ex1.py
+++ 2sem/manipulando-arquivos/man-ex1.py
@@ -3,8 +3,9 @@ import re
 try:
     f = open('D:/estudandopy/2sem/manipulando-arquivos/lero.txt','r', encoding='utf-8')
     # 96 seria quantidade de caracteres, no caso se fosse 10 iria apenas os 10 
-    # f.readline() - le cada linha, da pra colocar varios em print (em casos de duvida ver no versionameto)
-    print(f.readline(),f.readline())
+    # f.readline() - le cada linha, da pra colocar varios em print (em casos de duvida ver no versionameto) - inclusive da prsa fazer com a in range - ver2
+    for i in range(0,3,1):
+        print(f.readline())
     f.close()
 except FileNotFoundError:
     print("Arquivo n encontrado")
