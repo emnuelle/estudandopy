5b3ec9c3c98d6656687947fdd99640168afaa9cc
emnuelle <rm97973@fiap.com.br>
2023-09-04 10:57:02 -0300

    lero elro deu certo

--- 2sem/manipulando-arquivos/man-ex1.py
+++ 2sem/manipulando-arquivos/man-ex1.py
@@ -1,6 +1,11 @@
-f = open('lero.txt','r', encoding='utf-8')
-print(f.read())
-f.close()
+import re
+
+try:
+    f = open('D:/estudandopy/2sem/manipulando-arquivos/lero.txt','r', encoding='utf-8')
+    print(f.read())
+    f.close()
+except FileNotFoundError:
+    print("Arquivo n encontrado")
 
 # supostamente deveria printar o poema da baatinha :(
 # funcionou no pycharm!!!!!!! (pasta no dados(D))
