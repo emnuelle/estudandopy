60771bfc9b421c0cbadd03ffb9e76e9dacd5456c
emnuelle <rm97973@fiap.com.br>
2023-09-04 11:30:50 -0300

    lerolero

--- 2sem/manipulando-arquivos/man-ex1.py
+++ 2sem/manipulando-arquivos/man-ex1.py
@@ -1,11 +1,14 @@
 import re
 
 try:
+    f = open('D:/estudandopy/2sem/manipulando-arquivos/lero.txt','a', encoding='utf-8')
+    f.write("\n não sou pequeneninha")
+    f.write("\n do tamanho de um botão")
+
     f = open('D:/estudandopy/2sem/manipulando-arquivos/lero.txt','r', encoding='utf-8')
     # 96 seria quantidade de caracteres, no caso se fosse 10 iria apenas os 10 
     # f.readline() - le cada linha, da pra colocar varios em print (em casos de duvida ver no versionameto) - inclusive da prsa fazer com a in range - ver2
-    for i in range(0,3,1):
-        print(f.readline())
+    print(f.read())
     f.close()
 except FileNotFoundError:
     print("Arquivo n encontrado")
